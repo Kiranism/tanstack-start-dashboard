3243d28a3e8ee994215318f561ee9b0c0b5046ac
Thiwain Medagama <thiwainm@gmail.com>
2024-11-16 22:32:40 +0530

    made profile and themetoggle visible

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -19,7 +19,7 @@ export default function DashboardLayout({
       />
       <Sidebar />
       <div className="flex w-0 flex-1 flex-col overflow-hidden">
-        <div className="relative z-10 flex h-20 flex-shrink-0 md:hidden">
+        <div className="relative z-10 flex h-20 flex-shrink-0">
           <button
             className="pl-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 xl:hidden"
             onClick={() => setSidebarOpen(true)}
